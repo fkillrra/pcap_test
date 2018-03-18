aee05fe8376711a39af57c0e6cf87191add6145c
fkillrra <f.killrra@gmail.com>
2018-03-18 04:37:54 -0700

    Fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include <netinet/tcp.h>  //tcp header
 #include <arpa/inet.h>    //inet_ntoa()
 
-struct ethhdr *ethh;
+struct ether_header *ethh;
 struct ip *iph;
 struct tcphdr *tcph;
+struct udphdr *udph;
 
 void callback(u_char* handle, const struct pcap_pkthdr* header, const u_char* packet);
 void usage()
@@ -46,7 +47,7 @@ void callback(u_char* handle, const struct pcap_pkthdr* header, const u_char* pa
 {
   // Ethernet header
   int i;
-  ethh = (struct ethhdr *)packet;
+  ethh = (struct ether_header *)packet;
   printf("============== Ethernet ==============\n");
   printf("[Dst Mac address] : ");
   for(i = 0; i < 6; i++)
@@ -66,26 +67,27 @@ void callback(u_char* handle, const struct pcap_pkthdr* header, const u_char* pa
   printf("\n");
 
   // IP header
-  packet += sizeof(struct ethhdr); 
+  packet += sizeof(struct ether_header); //sizeof(struct eth); 
   iph = (struct ip *)packet;
   printf("================= IP =================\n");
-  printf("[Src IP address] : %s\n",inet_ntoa(iph -> ip_src));
+  printf("[Src IP address] : %s\n",inet_ntoa(iph -> ip_src));	//inet_ntoa() -> number to string
   printf("[Dst IP address] : %s\n",inet_ntoa(iph -> ip_dst));
 
   // TCP header
-  packet += (iph -> ip_hl * 4);
-    tcph = (struct tcphdr *)(packet + iph -> ip_hl * 4);
+  packet += 20; //(iph -> ip_hl * 4);		// packet length (total lenght -> 5 * 4 = 20)
+  tcph = (struct tcphdr *)packet;
   printf("================ TCP ================\n");
-  printf("[Src Port] : %d\n" , ntohs(tcph -> th_sport));
-    printf("[Dst Port] : %d\n" , ntohs(tcph -> th_dport));
+  printf("[Src Port] : %d\n" , ntohs(tcph -> th_sport));	//ntohs() -> network to host type : short
+  printf("[Dst Port] : %d\n" , ntohs(tcph -> th_dport));	// short : 2 byte
 
-    // Packet Data
-    printf("================ Data ================\n");
-    for(i = 0; i < 14; i++)
-    {
-      printf("%02x", *(packet++));
-      if(i % 14 == 0 && i != 0)
-         printf("\n");
-    }
-    printf("\n\n\n");
+  // Packet Data
+  packet += (tcph -> th_off);
+  printf("================ Data ================\n");
+  for(i = 0; i < 14; i++)
+  {
+    printf("%02x", *(packet++));
+    if(i % 14 == 0 && i != 0)
+       printf("\n");
+  }
+  printf("\n\n\n");
 }
